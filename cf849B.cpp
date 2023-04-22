b7350fc57be04018e55c4c4de2fc14110181ec65
joaoreis28 <reisjoaomendes@gmail.com>
2023-04-22 19:12:31 -0300

    Treinos.

--- cf849B.cpp
+++ cf849B.cpp
@@ -1,54 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void TestCase()
+{
+    int n, x, y, candy;
+    string s;
+
+    candy = 0;
+    x = 0;
+    y = 0;
+
+    cin >> n;
+    cin >> s;
+    
+    for(int i = 0; i < n; i++)
+    {
+        if(s[i] == 'U')
+            y = y + 1;
+        if(s[i] == 'L')
+            x -= 1;
+        if(s[i] == 'R')
+            x += 1;
+        if(s[i] == 'D')
+            y -= 1;
+        if(x == 1 && y == 1)
+            candy = 1;
+    }
+    if(candy)
+        printf("YES\n");
+    else
+        printf("NO\n");
+    
+}
+
+
 int main(void)
 {
-    int t;
-    int f;
-    int candy = 0;
-    int pos[2] = {0, 0};
+    int t;   
     scanf("%d", &t);
     while (t--)
     {
-        int n;
-        scanf("%d", &n);
-        candy = 0;
-        pos[0] = 0;
-        pos[1] = 0;
-        string s;
-        cin >> s;
-        
-        for(int i = 0; i < n; i++)
-        {
-            
-
-
-
-            if(s[i] == 'U')
-                pos[1] = pos[1] + 1;
-            if(s[i] == 'L')
-                pos[0] = pos[0] - 1;
-            if(s[i] == 'R')
-                pos[0] = pos[0] + 1;
-            if(s[i] == 'D')
-                pos[1] = pos[1] - 1;
-
-
-
-            if(pos[0] == 1 && pos[1] == 1)
-                candy = 1;
-        }
-        if(candy)
-            printf("YES\n");
-        else
-            printf("NO\n");
+     TestCase();   
         
     }
-    
-
-
-
-
 
     return 0;
 }
